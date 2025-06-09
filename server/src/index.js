42cd70dfe40261d2a1fc3a5760360872a35c8132
yousra <yousra@yousras-mbp.home>
2025-06-09 23:56:58 +0200

    add

--- server/src/index.js
+++ server/src/index.js
@@ -12,12 +12,32 @@ if (port == null) {
   logError(new Error("Cannot find a PORT number, did you create a .env file?"));
 }
 
+if (process.env.NODE_ENV === "production") {
+  app.use(
+    express.static(new URL("../../client/dist", import.meta.url).pathname)
+  );
+  app.get("*", (req, res) =>
+    res.sendFile(
+      new URL("../../client/dist/index.html", import.meta.url).pathname
+    )
+  );
+}
 
 app.listen(port, () => {
   logInfo(`Server started on port ${port}`);
 });
 
-
+// Host client code for Heroku
+if (process.env.NODE_ENV === "production") {
+  app.use(
+    express.static(new URL("../../client/dist", import.meta.url).pathname),
+  );
+  app.get("*", (req, res) =>
+    res.sendFile(
+      new URL("../../client/dist/index.html", import.meta.url).pathname,
+    ),
+  );
+}
 
 // For Cypress testing
 if (process.env.NODE_ENV !== "production") {
