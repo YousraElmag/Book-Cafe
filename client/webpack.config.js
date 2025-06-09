2b17a91fbcdb0b331e6129a566fa683608f5e4a7
yousra <yousra@yousras-mbp.home>
2025-06-09 02:47:57 +0200

    add

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -55,7 +55,7 @@ module.exports = {
     historyApiFallback: true,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        
         changeOrigin: true,
         secure: false,
       },
